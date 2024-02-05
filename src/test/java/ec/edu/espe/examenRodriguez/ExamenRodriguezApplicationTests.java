af479115c0c2924c7dd324d613ce9e24419dbfc2
Carlos Andres Rodriguez Preciado <carodriguez19@espe.edu.ec>
2024-02-05 17:46:58 -0500

    configuración inicial
    configuración inicial

---
+++ src/test/java/ec/edu/espe/examenRodriguez/ExamenRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examenRodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
